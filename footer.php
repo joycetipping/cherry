ce0e54959e0ca68fd5ed45a0ac95007551262f92
Joyce Tipping <joycetipping@rose.(none)>
2010-07-29 16:11:55 -0600

    Tweaked footer.

--- footer.php
+++ footer.php
@@ -16,18 +16,18 @@
 <?php } ?>
 -->
 
-  <div>
+  <p>
     <a href='http://joycetipping.com/'>joycetipping.com</a> is provided under the <a href='http://creativecommons.org/licenses/by-sa/3.0/'>Creative Commons Attribution-ShareAlike</a> license.
     <a href="http://validator.w3.org/check?uri=referer" title="Validate the XHTML of this page.">XHTML</a>, 
     <a href="http://jigsaw.w3.org/css-validator/check/referer" title="Validate the CSS of this page.">CSS</a>.
-  </div>
+  </p>
 
-  <div>
+  <p>
     Powered by <a href="http://wordpress.org/">WordPress</a> <?php bloginfo('version'); ?> |
     Cherry is my version of <a href="http://www.ardamis.com/2007/06/03/apricot/">Apricot</a>, an excellent theme by <a href="http://www.ardamis.com/">Ardamis.com</a>.
-  </div>
+  </p>
   
-  <div><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.</div>
+  <p><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.</p>
     <!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
   
 </div>
