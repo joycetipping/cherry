8150a715c378e3e1e6ddfea54f61075997bf0afb
Joyce Tipping <joycetipping@rose.(none)>
2010-08-01 13:17:31 -0600

    Tweaked the attachment template.

--- attachment.php
+++ attachment.php
@@ -4,10 +4,12 @@
 
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+  <!--
 		<div class="navigation">
 			<div class="alignleft">&nbsp;</div>
 			<div class="alignright">&nbsp;</div>
 		</div>
+  -->
 
     <?php $attachment_link = get_the_attachment_link($post->ID, true, array(450, 800)); // This also populates the iconsize for the next line ?>
     <?php $_post = &get_post($post->ID); $classname = ($_post->iconsize[0] <= 128 ? 'small' : '') . 'attachment'; // This lets us style narrow icons specially ?>
