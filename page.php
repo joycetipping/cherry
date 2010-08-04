c39cc7593ccb18656f49ab7a427f82488c6162b3
Joyce Tipping <joyce@joycetipping.com>
2010-08-04 17:45:03 -0600

    Commented out last modified date/time on pages. Tweaked .pagetitle in stylesheet to only remove top margin & padding, not all margin and padding.

--- page.php
+++ page.php
@@ -1,25 +1,25 @@
 <?php get_header(); ?>
 
-	<div id="content" class="narrowcolumn">
+  <div id="content" class="narrowcolumn">
 
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		<div class="post" id="post-<?php the_ID(); ?>">
-			<h1 class='pagetitle'><?php the_title(); ?></h1>
-			<p class="postdate"><?php the_modified_time('l, j F Y') ?></p>
+    <div class="post" id="post-<?php the_ID(); ?>">
+      <h1 class='pagetitle'><?php the_title(); ?></h1>
+<!--  <p class="postdate"><?php the_modified_time('l, j F Y') ?></p>-->
 
-			<div class="entry">
-				<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+      <div class="entry">
+        <?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
 
-				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
-			</div>
+        <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+      </div>
 
-		</div>
-		<?php endwhile; endif; ?>
+    </div>
+    <?php endwhile; endif; ?>
 
-	<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+  <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
 
-	</div>
+  </div>
 
 <?php get_sidebar(); ?>
 
